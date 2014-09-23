47b97b211ecafd2079cfe6090d4374caae7a8a20
Tomcha <tomcha@tomcha.net>
2014-09-23 16:16:08 +0900

    cssの調整とデフォルト日付の追加、入力値がエラーの時の処理を追加

--- app/mainapp.rb
+++ app/mainapp.rb
@@ -3,6 +3,7 @@
 
 require 'sinatra/base'
 require 'haml'
+require 'date'
 require_relative './lib/howold'
 
 Httpd_log = '~/lruby/mokumoku/howold/log/http_log.txt'
@@ -11,13 +12,17 @@ class MainApp < Sinatra::Base
   set :public_folder, File.expand_path(File.join(root, '..', 'public'))
 
   get '/' do
-    @agevalue = ''
+    td = Date.today
+    @today = td.strftime("%Y-%m-%d")
     haml :index
   end
 
   post'/howold' do
-    birthday = date_validate(@params[:birthday])
+    birthday = date_validate(@params[:birthday]) 
     basedate = date_validate(@params[:basedate])
+    unless(birthday && basedate)
+      redirect '/'
+    end
     @agevalue = age_calculate(birthday, basedate)
     @basedate = "#{basedate[0]}年#{basedate[1]}月#{basedate[2]}日"
     haml :howold
